8e84decb7e6b9420b90e68cde0139a7a3211155a
sodonnell <sean@seanodonnell.com>
2018-02-26 19:48:25 -0800

    Initial import; Basic MySQL Database Schema and Table Structure for the plowbot_logs database table.

---
+++ plowbot.sql
@@ -0,0 +1,16 @@
+/** plowbot mysql database schema and default table(s) */
+CREATE DATABASE IF NOT EXISTS `plowbot`;
+
+USE plowbot;
+
+CREATE TABLE `plowbot_logs` (
+  `id` mediumint(11) NOT NULL auto_increment,
+  `nick` varchar(25) default NULL,
+  `address` varchar(100) NOT NULL,
+  `chan` varchar(50) default NULL,
+  `server` varchar(50) default NULL,
+  `textinput` varchar(255) default NULL,
+  `logstamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  KEY `textinput` (`textinput`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='IRC Channel Logging Table for the PlowBot Perl Script.';
