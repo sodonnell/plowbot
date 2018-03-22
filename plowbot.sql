647320e1dbe7fb7e61dff51ebc5e9a5c6cf667d1
Sean O'Donnell <sean@seanodonnell.com>
2018-03-22 11:19:49 -0700

    Update plowbot.sql

--- plowbot.sql
+++ plowbot.sql
@@ -5,25 +5,25 @@ USE plowbot;
 
 CREATE TABLE IF NOT EXISTS `plowbot_logs` (
   `id` mediumint(11) NOT NULL auto_increment,
-  `nick` varchar(25) default NULL,
+  `nickname` varchar(25) default NULL,
   `address` varchar(100) NOT NULL,
-  `chan` varchar(50) default NULL,
+  `channel` varchar(50) default NULL,
   `server` varchar(50) default NULL,
-  `textinput` varchar(255) default NULL,
-  `logstamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `message` TEXT NOT NULL,
+  `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`),
-  KEY `textinput` (`textinput`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='IRC Channel Logging Table for the PlowBot Perl Script.';
+  KEY `message` (`message`)
+) ENGINE=InnoDB COMMENT='IRC Channel Logging Table for the PlowBot Perl Script.';
 
 CREATE TABLE IF NOT EXISTS `plowbot_quotes` (
 `id` int(11) NOT NULL auto_increment,
 `quote` TEXT NOT NULL,
 `nickname` varchar(50) NOT NULL,
 `channel` varchar(50) NOT NULL,
-`network` varchar(50) NOT NULL,
+`server` varchar(50) NOT NULL,
 `created_at` timestamp NOT NULL default CURRENT_TIMESTAMP,
 PRIMARY KEY  (`id`),
 KEY `nickname` (`nickname`),
 KEY `channel` (`channel`),
 KEY `network` (`network`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB COMMENT='IRC Channel Quote Table for the PlowBot Perl Script.';
